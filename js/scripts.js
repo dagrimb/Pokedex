78830b49d886cfddc78f96d13d822ba49fc0b620
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-13 08:48:59 -0800

    add pokemonList objects/arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 
-alert('Hello world');
-
-let favoriteFood = 'Ice Cream';
-
-document.write(favoriteFood)
-
+let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
+                {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
+                {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
