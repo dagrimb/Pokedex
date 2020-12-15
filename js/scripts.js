54d7ff2cb7981d3779138a0d92236d1dc33a2f62
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-14 21:25:16 -0800

    add first revision of conditional for first object in array

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@
 let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
                 {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
                 {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
+
+
+for (let i = 1; i < pokemonList.length; i++) {
+ 
+ if (pokemonList[i].height === 7 && pokemonList[i].types === 'grass' && 'poison') {
+    console.log(pokemonList[i].height + "is Bulbasaur");
+ }
+}
