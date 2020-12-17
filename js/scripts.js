e84f16c464d477649103c042c0327ae82c5673a1
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-16 17:40:55 -0800

    revised Balb name/height to console

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,10 @@ let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
                 {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
 
    for (var i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + '); ');
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "); ");
     //This writes Pokemon's height next to it's name//
  }
-
- if (height > 1.6) {
-    console.log(pokemonList[0].name + ' (height: ' + pokemonList[0].height + ') - Great Caeser\'s ghost...that is big!!!');
-    //This print's Bulbasaur's name and height to the console if the conditions are met//
+      if (height > 1.6) {
+         console.log(pokemonList[0].name + " (height: " + pokemonList[0].height + ") - Wow, that\'s big!");
+         //This print's Bulbasaur's name and height to the console if the conditions are met//
  }
