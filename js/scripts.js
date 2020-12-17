b5ef9e334b156b76b4028370b1bb9ea58bac0627
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-16 20:57:27 -0800

    add else statement to account for objects not singled out

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
                 {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
                 {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
 
-   for (var i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "); ");
-    //This writes Pokemon's height next to it's name//
-    if (pokemonList[i].height > 1.6) {
-      document.write(pokemonList[0].name + " (height: " + pokemonList[0].height + ") - Wow, that\'s big!");
-       //This print's Bulbasaur's name and height to the console if the conditions are met//
-   }
- }
+    for (var i = 0; i < pokemonList.length; i++) {
+        if (pokemonList[i].height > 1.6) {
+            document.write(pokemonList[0].name + " (height: " + pokemonList[0].height + ") - Wow, that\'s big!");
+            //This print's Bulbasaur's name and height to the console if the conditions are met//
+        } else {
+            document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "); ");
+            //This writes Pokemon's height next to it's name//
+    }
+}
+
