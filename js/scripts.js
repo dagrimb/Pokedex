235b50ddaccc40f7369d694319aadc4e94be0fcc
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-16 16:57:51 -0800

    wrote pokemon's height next to its name

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,6 @@ let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
                 {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
                 {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
 
-
-for (let i = 1; i < pokemonList.length; i++) {
- 
- if (pokemonList[i].height === 7 && pokemonList[i].types === 'grass' && 'poison') {
-    console.log(pokemonList[i].name + "is Bulbasaur");
- } else if (pokemonList[i].height === 1.6 && pokemonList[i].types === 'ground', 'rock', 'electric', 'ice', 'psychic') {
-    console.log(pokemonList[i].name + "is Golbat");
- } else {
-    console.log(pokemonList[i].name + "is Shellder")
- }
+   for (var i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name);
  }
