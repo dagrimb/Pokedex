fadd78234338854a3435fb2d4455af96a185fafe
dagrimb <74441727+dagrimb@users.noreply.github.com>
2020-12-16 17:33:24 -0800

    add code that print's Bulbasaur's name and height to console if conditions are met

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,8 @@ let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + '); ');
     //This writes Pokemon's height next to it's name//
  }
+
+ if (height > 1.6) {
+    console.log(pokemonList[0].name + ' (height: ' + pokemonList[0].height + ') - Great Caeser\'s ghost...that is big!!!');
+    //This print's Bulbasaur's name and height to the console if the conditions are met//
+ }
