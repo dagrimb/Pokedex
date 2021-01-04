9c3135abf6dfe39fba036378d750867d52299e49
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-03 21:15:52 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
 
 
-function executeProgram() {
+
     let pokemonRepository = (function () {
         let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
         {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
         {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
    
-    function getAll() {
-        return pokemonList; 
-    }
-
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
-
-    return {
-        getAll: getAll,
-        add: add
-    };
-})();
-    
-    executeProgram();
-
-    console.log(pokemonRepository.getAll());
-
-    pokemonList.forEach(function(pokemon) {
-        if (pokemon.height > 1.6) {
-            document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that\'s big!</p>");
-            //This print's Bulbasaur's name and height to the console if the conditions are met//
-        } else {
-            document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") </p>");
-            //This writes Pokemon's height next to it's name//
+        function getAll() {
+            return pokemonList; 
         }
-    });
-}
+
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+
+        return {
+            getAll: getAll,
+            add: add
+        };
+    })();
+    
+    pokemonRepository.getAll();
+
+        pokemonList.forEach(function(pokemon) {
+            if (pokemon.height > 1.6) {
+                document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that\'s big!</p>");
+                //This print's Bulbasaur's name and height to the console if the conditions are met//
+            } else {
+                document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") </p>");
+                //This writes Pokemon's height next to it's name//
+            }
+        });
+
