a2f04c01e7c2f6d67afb1e4368d372d122348cee
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-03 19:56:15 -0800

    correct assignment of IIFE to pokemonRepository variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 
 
 function executeProgram() {
-    let pokemonRepository = {};
-
-    let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
-                {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
-                {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
-
-    }
+    let pokemonRepository = (function () {
+        let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
+        {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
+        {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
+   
+   
+   
+   
+        )};
+    
 
     executeProgram();
 
