620e6c109cceb7b02e590527b8b483ef04f9717a
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-04 10:49:40 -0800

    create unordered list variable inside forEach block

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,7 @@
     })();
     
         pokemonRepository.getAll().forEach(function(pokemon) {
+   
             if (pokemon.height > 1.6) {
                 document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that\'s big!</p>");
                 //This print's Bulbasaur's name and height to the console if the conditions are met//
@@ -28,5 +29,7 @@
                 document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") </p>");
                 //This writes Pokemon's height next to it's name//
             }
+
+            let unorderedList = document.querySelector('ul');
         });
 
