17fc6c05a7398b54da239bea536ff4d451ee4dd3
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-03 19:47:44 -0800

    create pokemonRepository variable to hold what IIFE will return

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 
 function executeProgram() {
-    let data = {};
+    let pokemonRepository = {};
 
     let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
                 {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
@@ -10,6 +10,8 @@ function executeProgram() {
     }
 
     executeProgram();
+
+
 
     pokemonList.forEach(function(pokemon) {
         if (pokemon.height > 1.6) {
