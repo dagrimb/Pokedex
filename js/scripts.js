7e5ccd631a9a6d8cf158b3585add62131f21faef
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-06 09:06:50 -0800

    add comments to functions in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,29 @@
         let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
         {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
         {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
-   
+   //List of all pokemon
+
         function getAll() {
             return pokemonList; 
         }
+        //Returns array of all pokemon
 
         function add(pokemon) {
             pokemonList.push(pokemon);
         }
+        //Adds pokemon to array
 
         function showDetails(pokemon) {
             console.log(pokemon);
         }
+        //Logs details of pokemon to the JS console
 
         function addListener(button, pokemon) {
             button.addEventListener('click', function (event) {
                 showDetails(pokemon);
             })
         }
+        //Show details of pokemon in he JS console when pokemon is clicked in the UI
 
         function addListItem(pokemon) {
             let pokemonList = document.querySelector('.pokemon-list');
@@ -34,6 +39,7 @@
             pokemonList.appendChild(listItem);
             addListener(button, pokemon);
             }
+        //Create button
         
         return {
             getAll: getAll,
