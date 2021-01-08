34bf5a84ed4ce692285588ba699fd1324ef1ed6c
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-07 20:03:09 -0800

    replace array of Pokemon objects with empty array

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,7 @@
 
 
     let pokemonRepository = (function () {
-        let pokemonList = [{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']}, 
-        {name: 'Golbat', height: 1.6, types: ['ground', 'rock', 'electric', 'ice', 'psychic']},
-        {name: 'Shellder', height: 0.3, types: ['grass', 'electric']}];
-        //List of all pokemon
+        let pokemonList = [];
 
         let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
