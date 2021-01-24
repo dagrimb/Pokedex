c8eda8d4c6d1b1ea347f7ea83a70d3e88f617533
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-24 14:55:20 -0800

    add blue color to the via js

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,8 @@
         //Create button
         function addListItem(pokemon) {
             pokemonRepository.loadDetails(pokemon).then(function () {
+                let bodyStyles = $('body');
+                bodyStyles.addClass('p-3 mb-2 bg-primary');
                 let pokemonList = $('.pokemon-list');
                 pokemonList.addClass('list-group list-unstyled col-sm-6 card');    
                 let listItem = $('<li class="group-list-item card-body"></li');
